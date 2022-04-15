ed780ffb6d3fd991b7f133e80b9bd8a736627729
Ben-Ryder <dev@benryder.me>
2022-04-15 10:26:25 +0100

    chore: moving tailwind.css

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,4 @@
-import '../src/styles/tailwind.css';
+import '../src/tailwind.css';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './styles/tailwind.css';
+import './tailwind.css';
 
 function App() {
   return (
