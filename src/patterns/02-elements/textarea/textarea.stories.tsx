6e6b17dae48a0afd6ad58b90d642c9b4868d0869
Ben Ryder <dev@benryder.me>
2022-05-03 19:56:03 +0100

    feat: adding textarea component

---
+++ src/patterns/02-elements/textarea/textarea.stories.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { TextArea } from './textarea';
+
+export default {
+  title: 'TextArea',
+  component: TextArea
+} as ComponentMeta<typeof TextArea>;
+
+const Template: ComponentStory<typeof TextArea> = (args) => <TextArea {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  id: "default",
+  label: "This is a text area"
+};
+
+export const HiddenLabel = Template.bind({});
+HiddenLabel.args = {
+  id: "default",
+  hideLabel: true,
+  label: "This label can't be seen"
+};
---
+++ src/patterns/02-elements/textarea/textarea.tsx
@@ -0,0 +1,38 @@
+import React, { ComponentProps, forwardRef } from 'react';
+import classNames from "classnames";
+
+import { ErrorText } from "../error-text/error-text";
+import { Label } from '../label/label';
+
+export interface TextAreaProps extends ComponentProps<'textarea'> {
+  id: string,
+  label: string,
+  error?: string,
+  hideLabel?: boolean,
+}
+
+export const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>((props, ref) => {
+  return (
+    <>
+      {props.label && props.id &&
+          <Label htmlFor={props.id} isHidden={props.hideLabel || false}>{ props.label }</Label>
+      }
+      <textarea
+        {...props}
+        className={classNames(
+          "mt-1 block w-full rounded-md outline-none py-1.5 bg-br-atom-700",
+          "border-2 text-br-whiteGrey-200",
+          "focus:ring-0 focus:border-br-teal-600",
+          {
+            "border-br-red-500 hover:border-br-red-600": props.error,
+            "border-br-blueGrey-600 hover:border-br-blueGrey-500": !props.error
+          },
+          props.className,
+        )}
+      />
+      {props.error &&
+          <ErrorText>{props.error}</ErrorText>
+      }
+    </>
+  )
+});
