af37da0e3130c4e9418743a457526bc7f2f604d2
Ben Ryder <dev@benryder.me>
2023-01-09 18:44:57 +0000

    fix: fixing assets not being included in build

--- packages/core/gulpfile.js
+++ packages/core/gulpfile.js
@@ -4,7 +4,8 @@ const rename = require('gulp-rename');
 function buildFonts() {
     return src('./patterns/**/*.ttf')
         .pipe(rename({dirname: ''}))
-        .pipe(dest('./dist/fonts'));
+        .pipe(dest('./dist/fonts'))
+        .pipe(dest('./build/fonts'));
 }
 
 exports.assets = parallel(buildFonts);
