fa0e7b40924e3952683777e69894e5877476c16b
Ben Ryder <dev@benryder.me>
2023-04-09 21:00:28 +0100

    feat: adding initialCount prop to counter

--- packages/react/src/02-components/counter/counter.tsx
+++ packages/react/src/02-components/counter/counter.tsx
@@ -4,11 +4,12 @@ import {JButton} from "../../01-atoms/button/button.js";
 
 export interface CounterProps {
 	title: string,
-	body: string
+	body: string,
+	initialCount?: number
 }
 
 export function JCounter(props: CounterProps) {
-	const [count, setCount] = useState<number>(0);
+	const [count, setCount] = useState<number>(props.initialCount || 0);
 
 	return (
 		<div className="j-counter">
