1068772fc12e1632484abf735207d0c6a2f10269
Ben Ryder <dev@benryder.me>
2023-12-09 17:29:02 +0000

    fix: fixing select text getting cut off
    Refs: #55

--- packages/react/src/01-atoms/forms/multiselect/multiselect.stories.tsx
+++ packages/react/src/01-atoms/forms/multiselect/multiselect.stories.tsx
@@ -77,6 +77,7 @@ export function Minimal() {
       selectedOptions={selectedOptions}
       setSelectedOptions={setSelectedOptions}
       variant="minimal"
+      hideLabel={true}
     />
   );
 }
