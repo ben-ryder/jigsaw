bf97f71514f93fae5ec944c86dfc1779d144d5b1
Ben Ryder <dev@benryder.me>
2023-12-11 20:38:27 +0000

    release(react): v0.17.1 - fixing issue where colour variants could no longer be directly referenced

--- packages/react/src/00-foundations/colours/variants/colour-variants.ts
+++ packages/react/src/00-foundations/colours/variants/colour-variants.ts
@@ -1,14 +1,27 @@
+export const JColourVariants = {
+	teal: "teal",
+	blueGrey:"blueGrey",
+	white: "white",
+	red: "red",
+	orange: "orange",
+	yellow: "yellow",
+	green: "green",
+	blue: "blue",
+	purple: "purple",
+	pink: "pink",
+}
+
 export const JColourVariantsList = [
-	"teal",
-	"blueGrey",
-	"white",
-	"red",
-	"orange",
-	"yellow",
-	"green",
-	"blue",
-	"purple",
-	"pink",
+	JColourVariants.teal,
+	JColourVariants.blueGrey,
+	JColourVariants.white,
+	JColourVariants.red,
+	JColourVariants.orange,
+	JColourVariants.yellow,
+	JColourVariants.green,
+	JColourVariants.blue,
+	JColourVariants.purple,
+	JColourVariants.pink,
 ] as const;
 
 export type JColourVariants = typeof JColourVariantsList[number]
